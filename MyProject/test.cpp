1f3d222cbed21dbb0b7a4af72b520ae37e953d4a
Menh1505 <dinhthienmenh1505@gmail.com>
2023-11-26 03:12:33 +0700

    them file test

---
+++ MyProject/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    long double n, k;
+    cin >> n >> k;
+
+    cout << (long long)min(n, (1 + sqrt(1 - 4 * 3 * -2 * k)) / 6);
+
+    return 0;
+}
